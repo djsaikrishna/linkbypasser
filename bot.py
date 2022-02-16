8a10e3afd7af269324e2ee3fa27e1b74365cd1a7
Shivayt007 <88087547+Shivayt007@users.noreply.github.com>
2022-02-16 18:13:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,12 @@
 from os import environ
 import aiohttp
 from pyrogram import Client, filters
+import time
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+
 
 API_ID = environ.get('API_ID')
 API_HASH = environ.get('API_HASH')
