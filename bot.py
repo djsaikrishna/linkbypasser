9fdc45ae7888fb721abae4e4515368bac2ee6565
Shivayt007 <88087547+Shivayt007@users.noreply.github.com>
2022-02-17 08:35:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ async def link_handler(bot, message):
             bypass_link = await gplinks_bypass(link) 
             link_by = bypass_link.get('url')
             k = await message.reply(f"**Please Wait , Bot Is Processing The Link**")
-            await asyncio.sleep(9)
+            await sleep(9)
             await k.delete()
             await message.reply(f' **Here is your** : </b> \n\n {link_by}')
         except Exception as e:
@@ -46,7 +46,7 @@ async def link_handler(bot, message):
             bypass_link = await droplink_bypass(link) 
             link_by = bypass_link.get('url')
             k = await message.reply(f"**Please Wait , Bot Is Processing The Link**")
-            await asyncio.sleep(9)
+            await sleep(9)
             await k.delete()
             await message.reply(f' **Here is your** : </b> \n\n {link_by}')
         except Exception as e:
