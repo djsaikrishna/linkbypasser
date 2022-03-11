5ea2c84820410e543ff1b7f89186d078dbf77366
Shivayt007 <88087547+Shivayt007@users.noreply.github.com>
2022-03-11 20:59:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,31 +21,49 @@ bot = Client('gplink bot',
              workers=50,
              sleep_threshold=10)
 
+
+
 @bot.on_message(filters.command('start') & filters.private)
 async def start(bot, message):
     await message.reply(
         f"**Hai Nanba  {message.chat.first_name}!**\n\n"
         "**Hey I Am By_Passer Bot Don't Flood A Bot ,Don't Use This Bot Without Getting Permission From Developer**.\n\n **Credits : XCSCXR For His Script**   \n\n **Dev By : @LoveToRide**  ")
 
+
 @bot.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
 async def link_handler(bot, message):
     link = message.matches[0].group(0)
-    
-    # this function for #linkvertise
-    if "linkvertise" in link:
-        try:
+    link = f"{message.text}"
+    k = await message.reply(f"**Please Wait , Bot Is Processing 🔑 The Link {message.text}**")
+    URL = link
+    r = requests.get(URL)
 
-            link = f"{message.text}"
-            k = await message.reply(f"**Please Wait , Bot Is Processing 🔑 The Link {message.text}**")
-            bypass_link =  lv_bypass(link)
-            bypass_link = bypass_link
-            await asyncio.sleep(9)
-            await k.delete()
-            txt = f'**🧨ByPassed Url**:</b>**{bypass_link}****\n\n💣Non_Bypassed Url :{message.text}**\n\n**⭕️Bot_Started By : @{message.chat.username} / {message.chat.id} **\n\n⭕️**Powered By: @TRVPN**'
-            await message.reply(txt, quote=True)
-            await bot.send_message(LOG_CHANNEL, txt)
-        except Exception as e:
-            await message.reply(f'{e}', quote=True)
+    r = r.text
 
-   
+    soup = BeautifulSoup(r,'html.parser')
+
+    tr = []
+    for link in soup.find_all('a'):
+        list1 = link.get('href')
+        tr.append(list1)
+
+
+    b = []
+    for i in tr:
+
+        if i is None :
+
+           pass
+        else:
+            b.append(i)
+    for ge in b:
+
+        if 'applications' in ge:
+            zap = f"Torrent File Link :\n\n {ge}"
+            await message.reply(zap, quote=True)
+        elif 'magnet' in ge:
+            sap = f"Magnetic Link :\n\n {ge}"
+            await message.reply(zap, quote=True)
+            
+            
 bot.run()
